0368dbd7cb7b805f94f6b6dee6893fcdae6a19bd
Poujhit <poujhit.dev@gmail.com>
2023-11-17 15:01:22 +0530

    added models for all aspect of the app

--- server/api/admin.py
+++ server/api/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 
-from api.models import User
+from api.models import User, Album, Artist, Genre, Playlist, Track, UserRating
 
 # Register your models here.
 
 admin.site.register(User)
+admin.site.register(Album)
+admin.site.register(Artist)
+admin.site.register(Genre)
+admin.site.register(Playlist)
+admin.site.register(Track)
+admin.site.register(UserRating)
--- server/api/models.py
+++ server/api/models.py
@@ -7,6 +7,96 @@ class User(models.Model):
     username = models.CharField(max_length=255)
     email = models.EmailField(unique=True)
     password = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.username
+
+
+class Artist(models.Model):
+    artist_name = models.CharField(max_length=255, unique=True)
+    image_url = models.URLField(blank=False, null=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.artist_name
+
+
+class Genre(models.Model):
+    genre_name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.genre_name
+
+
+class Album(models.Model):
+    album_title = models.CharField(max_length=255)
+    artists = models.ManyToManyField(Artist)
+    release_date = models.DateField()
+    cover_image_url = models.URLField(blank=False, null=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.album_title} by {self.artist.artist_name}"
+
+
+class Track(models.Model):
+    track_title = models.CharField(max_length=255)
+    track_image_url = models.URLField(blank=False, null=False)
+    album = models.ForeignKey(Album, on_delete=models.CASCADE)
+    artists = models.ManyToManyField(Artist)
+    release_date = models.DateField()
+    genre = models.ForeignKey(
+        Genre, on_delete=models.SET_NULL, null=True, blank=False)
+    audio_url = models.URLField(blank=False, null=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.track_title} - {', '.join(artist.artist_name for artist in self.artists.all())}"
+
+
+class Playlist(models.Model):
+    username = models.ForeignKey(User, on_delete=models.CASCADE)
+    playlist_name = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    tracks = models.ManyToManyField(Track)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    # System should be able to auto suggest songs to a user based on the songs that are present in their playlist.
+    # Can match to a genre/album/artist
+    def suggest_songs(self, num_suggestions=5):
+        playlist_tracks = self.tracks.all()
+
+        genres = set(track.genre for track in playlist_tracks if track.genre)
+        albums = set(track.album for track in playlist_tracks if track.album)
+        artists = set(
+            artist for track in playlist_tracks for artist in track.artists.all())
+
+        suggested_songs = Track.objects.filter(
+            models.Q(genre__in=genres) |
+            models.Q(album__in=albums) |
+            models.Q(artists__in=artists)
+        ).exclude(id__in=[track.id for track in playlist_tracks]).distinct()[:num_suggestions]
+
+        return suggested_songs
+
+    def __str__(self):
+        return self.playlist_name
+
+
+class UserRating(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    track = models.ForeignKey(Track, on_delete=models.CASCADE)
+    rating = models.PositiveIntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        # constraint to ensure that each user can rate each song only once
+        unique_together = ('user', 'track')
+
+    def __str__(self):
+        return f"{self.user.username} rated {self.track.track_title} with {self.rating} stars"
