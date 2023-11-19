c6812179803af79eabd1a4f15f2d0b20a231c717
Poujhit <poujhit.dev@gmail.com>
2023-11-19 11:55:39 +0530

    deploying using render.com

--- server/backend/settings.py
+++ server/backend/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-nu=l@@f2nk9j4^y_61z925hcq31en=(d(*khunh(@cqa$nc!&*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
--- server/backend/wsgi.py
+++ server/backend/wsgi.py
@@ -16,5 +16,3 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 
 application = get_wsgi_application()
-
-app = application
